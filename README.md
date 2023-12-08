9e8032afe2ee9c04bf1b7c0963e38f4de93994f4
Miguel Angel Sanchez Palafox <pala_fox044@hotmail.com>
2023-12-08 11:57:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Model Training and Inference with Python
+
+This Python script allows users to either train a model or perform inference using a pre-trained model.
+
+## Requirements
+
+- Python 3.x
+- Required Python packages: `numpy`, `torch`, `datasets`, `transformers`, `scikit-learn`
+
+You can install the required packages using pip:
+
+```bash
+pip install numpy torch datasets transformers scikit-learn transformers[torch]
+```
+
+## Usage
+- Clone or download the repository.
+- Navigate to the directory containing the Python script.
+- Run the script using Python:
+
+```bash
+python test.py
+```
+
+## Options
+When prompted, enter yes to train the model.
+If you choose not to train the model, you'll be prompted to enter text for inference.
---
+++ test.py
@@ -0,0 +1,132 @@
+import numpy as np
+import torch
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, EvalPrediction
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
+
+# The functions and dataset loading code from your provided script
+
+dataset = load_dataset("sem_eval_2018_task_1", "subtask5.english")
+labels = [label for label in dataset['train'].features.keys() if label not in ['ID', 'Tweet']]
+id2label = {idx:label for idx, label in enumerate(labels)}
+label2id = {label:idx for idx, label in enumerate(labels)}
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+def preprocess_data(examples):
+  # take a batch of texts
+  text = examples["Tweet"]
+  # encode them
+  encoding = tokenizer(text, padding="max_length", truncation=True, max_length=128)
+  # add labels
+  labels_batch = {k: examples[k] for k in examples.keys() if k in labels}
+  # create numpy array of shape (batch_size, num_labels)
+  labels_matrix = np.zeros((len(text), len(labels)))
+  # fill numpy array
+  for idx, label in enumerate(labels):
+    labels_matrix[:, idx] = labels_batch[label]
+
+  encoding["labels"] = labels_matrix.tolist()
+
+  return encoding
+
+encoded_dataset = dataset.map(preprocess_data, batched=True, remove_columns=dataset['train'].column_names)
+encoded_dataset.set_format("torch")
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased",
+                                                           problem_type="multi_label_classification",
+                                                           num_labels=len(labels),
+                                                           id2label=id2label,
+                                                           label2id=label2id)
+
+batch_size = 8
+metric_name = "f1"
+
+args = TrainingArguments(
+    f"bert-finetuned-sem_eval-english",
+    evaluation_strategy = "epoch",
+    save_strategy = "epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=5,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    metric_for_best_model=metric_name,
+    #push_to_hub=True,
+)
+
+
+# source: https://jesusleal.io/2021/04/21/Longformer-multilabel-classification/
+def multi_label_metrics(predictions, labels, threshold=0.5):
+    # first, apply sigmoid on predictions which are of shape (batch_size, num_labels)
+    sigmoid = torch.nn.Sigmoid()
+    probs = sigmoid(torch.Tensor(predictions))
+    # next, use threshold to turn them into integer predictions
+    y_pred = np.zeros(probs.shape)
+    y_pred[np.where(probs >= threshold)] = 1
+    # finally, compute metrics
+    y_true = labels
+    f1_micro_average = f1_score(y_true=y_true, y_pred=y_pred, average='micro')
+    roc_auc = roc_auc_score(y_true, y_pred, average = 'micro')
+    accuracy = accuracy_score(y_true, y_pred)
+    # return as dictionary
+    metrics = {'f1': f1_micro_average,
+               'roc_auc': roc_auc,
+               'accuracy': accuracy}
+    return metrics
+
+def compute_metrics(p: EvalPrediction):
+    preds = p.predictions[0] if isinstance(p.predictions,
+            tuple) else p.predictions
+    result = multi_label_metrics(
+        predictions=preds,
+        labels=p.label_ids)
+    return result
+
+
+#forward pass
+outputs = model(input_ids=encoded_dataset['train']['input_ids'][0].unsqueeze(0), labels=encoded_dataset['train'][0]['labels'].unsqueeze(0))
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=encoded_dataset["train"],
+    eval_dataset=encoded_dataset["validation"],
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
+
+# Function to train the model
+def train_model():
+    trainer.train()
+    trainer.evaluate()
+    # Code for training the model as in your provided script
+
+# Function for inference
+def perform_inference(text):
+    encoding = tokenizer(text, return_tensors="pt")
+    encoding = {k: v.to(trainer.model.device) for k,v in encoding.items()}
+
+    outputs = trainer.model(**encoding)
+    logits = outputs.logits
+    sigmoid = torch.nn.Sigmoid()
+    probs = sigmoid(logits.squeeze().cpu())
+    predictions = np.zeros(probs.shape)
+    predictions[np.where(probs >= 0.5)] = 1
+    # turn predicted id's into actual label names
+    predicted_labels = [id2label[idx] for idx, label in enumerate(predictions) if label == 1.0]
+    print(predicted_labels)
+
+
+# Ask user for choice
+choice = input("Do you want to train the model? (yes/no): ")
+
+if choice.lower() == "yes":
+    train_model()
+else:
+    # Perform inference
+    text_to_infer = input("Enter the text for inference: ")
+    # Call the inference function with user input text
+    perform_inference(text_to_infer)
